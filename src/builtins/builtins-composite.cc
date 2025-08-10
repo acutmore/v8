0173761f8340a6c11eefb7072210c80025c4f861
Ashley Claymore <aclaymore@bloomberg.net>
2025-08-10 16:05:13 +0100

    sameValueZero (normalize -0 to 0)

--- src/builtins/builtins-composite.cc
+++ src/builtins/builtins-composite.cc
@@ -79,6 +79,10 @@ BUILTIN(CompositeConstructor) {
           isolate, value, JSReceiver::GetProperty(isolate, Cast<JSReceiver>(input), key));
     }
 
+    if (IsHeapNumber(*value) && Cast<HeapNumber>(*value)->value() == 0) {
+      value = handle(Smi::FromInt(0), isolate);
+    }
+
     hashcode ^= key->hash();
     if (IsJSComposite(*value)) {
       DirectHandle<JSComposite> composite(Cast<JSComposite>(*value), isolate);
@@ -141,9 +145,7 @@ Tagged<Object> CompareComposites(Isolate* isolate,
       Tagged<Object> av = ac->RawFastPropertyAt(field_index);
       Tagged<Object> bv = bc->RawFastPropertyAt(field_index);
 
-      // For now just use strict equality comparison for property values
       if (IsJSComposite(av) && IsJSComposite(bv)) {
-        // Recursively compare composites
         if (CompareComposites(isolate,
               DirectHandle<JSComposite>(Cast<JSComposite>(av), isolate),
               DirectHandle<JSComposite>(Cast<JSComposite>(bv), isolate)) != ReadOnlyRoots(isolate).true_value()) {
